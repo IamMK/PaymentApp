1bb67da09dc67d8f937e6818c8768f2e728b46f4
IamMK <malk@int.pl>
2022-12-05 21:13:35 +0100

    Profile View change

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -3,28 +3,35 @@
     <top-bar>
       <h1>Profil</h1>
     </top-bar>
-    <base-notecard v-if="userInfoStore.isProfileFinished">
+    <base-notecard class="profile" v-if="userInfoStore.isProfileFinished">
       <h2>
         Hej,
         {{ userInfoStore.userInfo[ProfileField.NICKNAME].value }}
       </h2>
       <h3>Podsumujmy informacje, jakie o Tobie posiadam:</h3>
-      <p>
-        Typ wynagrodzenia:
-        {{ userInfoStore.userInfo[ProfileField.SALARYTYPE].value }}
-      </p>
-      <p>
-        Wynagrodzenie na umowie:
-        {{ userInfoStore.userInfo[ProfileField.SALARYAMOUNT].value }}
-        {{ userInfoStore.userInfo[ProfileField.CURRENCY].value
-        }}{{
-          userInfoStore.userInfo[ProfileField.SALARYTYPE].value ===
-          SalaryType.HOURLY
-            ? "/h"
-            : ""
-        }}
-      </p>
-      Język, jakiego używasz w PaymentApp to: {{ langStore.getLangName }}
+      <article>
+        <h4>Typ wynagrodzenia:</h4>
+        <p>
+          {{ userInfoStore.userInfo[ProfileField.SALARYTYPE].value }}
+        </p>
+      </article>
+      <article>
+        <h4>Wynagrodzenie na umowie:</h4>
+        <p>
+          {{ userInfoStore.userInfo[ProfileField.SALARYAMOUNT].value }}
+          {{ userInfoStore.userInfo[ProfileField.CURRENCY].value
+          }}{{
+            userInfoStore.userInfo[ProfileField.SALARYTYPE].value ===
+            SalaryType.HOURLY
+              ? "/h"
+              : ""
+          }}
+        </p>
+      </article>
+      <article>
+        <h4>Język, jakiego używasz to:</h4>
+        <p>{{ langStore.getLangName }}</p>
+      </article>
       <h3>Jeżeli będę czegoś jeszcze potrzebować, odezwę się do Ciebie</h3>
     </base-notecard>
     <profile-set v-else></profile-set>
@@ -43,3 +50,11 @@ import { useLangStore } from "@/store/lang";
 const userInfoStore = useUserInfo();
 const langStore = useLangStore();
 </script>
+
+<style lang="scss">
+.profile {
+  & h3 {
+    padding: 10px;
+  }
+}
+</style>
